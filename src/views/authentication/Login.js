973184491eab673c5654653d31d49623fd764d47
Ernest Ehigiator <ernestehigiator@yahoo.com>
2022-07-05 16:46:50 +0100

    Adjustment to login features

--- src/views/authentication/Login.js
+++ src/views/authentication/Login.js
@@ -1,7 +1,17 @@
+import Input from "../../components/reusables/Input"
+import emailIcon from "../../assets/email.svg"
+import passwordIcon from "../../assets/password.svg"
+
+const handleClick = (e) => {
+    console.log(e)
+}
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div>
+            <Input text="email" handleClick={handleClick} icon={emailIcon} label="email" />
+            <Input text="password" handleClick={handleClick} icon={passwordIcon} label="password" />
+        </div>
     )
 }
 
