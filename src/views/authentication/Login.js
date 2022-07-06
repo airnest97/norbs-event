8d1996de69806227cb1668d3e941dd8eeb9a4cfe
Ernest Ehigiator <ernestehigiator@yahoo.com>
2022-07-06 16:38:41 +0100

    Further improvement to the login page

--- src/views/authentication/Login.js
+++ src/views/authentication/Login.js
@@ -1,6 +1,7 @@
 import Input from "../../components/reusables/Input"
 import emailIcon from "../../assets/email.svg"
 import passwordIcon from "../../assets/password.svg"
+import "./login.css"
 
 const handleClick = (e) => {
     console.log(e)
@@ -8,10 +9,39 @@ const handleClick = (e) => {
 
 const Login = () => {
     return (
-        <div>
-            <Input text="email" handleClick={handleClick} icon={emailIcon} label="email" />
-            <Input text="password" handleClick={handleClick} icon={passwordIcon} label="password" />
+        <div className="loginContainer">
+            <div className="leftSide">
+                <div className="leftSide-container">
+                    <a>Don't have an account?
+                        <span style={{ 
+                            color: 'var(--primary_green)', 
+                            marginLeft: '4px'
+                        }}> 
+                            sign up
+                        </span>
+                    </a>
+                    <div className="welcome-text">
+                        <h1>Welcome To Norbs</h1>
+                        <p>We are an event management platform, aimed at helping 
+                            you facilitate and run a smooth event</p>
+                    </div>
+                    <div className="input-button-fields">
+                        <Input text="email" handleClick={handleClick} icon={emailIcon} label="email" />
+                        <Input text="password" handleClick={handleClick} icon={passwordIcon} label="password" />
+                        <button>Get Into Norbs</button>
+                    </div>
+                    <div className="social-media">
+                        <a>
+                            <div className="social-media-icon"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="rightSide">
+
+            </div>
         </div>
+       
     )
 }
 
